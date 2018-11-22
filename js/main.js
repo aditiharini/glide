6e500683e22e57e57cf9ddf45179096af422c0a6
Aditi Srinivasan <aditisri@mit.edu>
2018-11-22 14:07:51 -0500

    added letter A

--- js/main.js
+++ js/main.js
@@ -20,12 +20,15 @@ function updateCount() {
     var count = particles.statesize[0] * particles.statesize[1];
     $('.count').text(comma(count));
 }
+var alphabet = {
+    "A":new Letter([[0, 0], [25, 25], [50, 50], [75, 25], [100, 0], [50, 25]]),
+    "I": new Letter([[0, 0], [25, 0], [50, 0], [25, 25], [25, 50], [25, 75], [25, 100], [0, 100], [50, 100]]), 
+};
 
 $(document).ready(function() {
     var canvas = $('#display')[0];
     // Need to make sure anchor points are somewhat evenly spaced
-    letter = new Letter([[0, 0], [25, 0], [50, 0], [25, 25], [25, 50], [25, 75], [25, 100], [0, 100], [50, 100]]);
-    particles = new Particles(canvas, 1024, letter.scaled(2), 3).draw().start();
+    particles = new Particles(canvas, 1024, alphabet.A.scaled(2), 3).draw().start();
     controller = new Controller(particles);
     new FPS(particles);
     updateCount();
