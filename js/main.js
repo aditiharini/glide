35c76458b0b17b0c90bae7fd62c4b2d3554a734f
Aditi Srinivasan <aditisri@mit.edu>
2018-11-25 21:06:39 -0500

    curved letters working

--- js/main.js
+++ js/main.js
@@ -13,24 +13,43 @@ function comma(num) {
 }
 
 var particles = null,
-    controller = null,
-    letter = null;
+    controller = null;
 
 function updateCount() {
     var count = particles.statesize[0] * particles.statesize[1];
     $('.count').text(comma(count));
 }
-var alphabet = {
-    "A":new Letter([[0, 0], [25, 25], [50, 50], [75, 25], [100, 0], [50, 25]]),
-    "I": new Letter([[0, 0], [25, 0], [50, 0], [25, 25], [25, 50], [25, 75], [25, 100], [0, 100], [50, 100]]), 
-};
 
 $(document).ready(function() {
     var canvas = $('#display')[0];
-    // Need to make sure anchor points are somewhat evenly spaced
-    letters = [alphabet.A.translated(50, 100).scaled(3), alphabet.I.translated(200, 100).scaled(2)]
-    particles = new Particles(canvas, 1024 * letters.length, letters, 3).draw().start();
-    controller = new Controller(particles);
-    new FPS(particles);
-    updateCount();
+    var loader = new THREE.FontLoader();
+    loader.load(
+        'fonts/helvetiker_bold.typeface.json',
+        function ( font ) {
+            var letters = new THREE.TextGeometry("ADITI SAYS HI", {
+                font: font 
+            });
+            letters.computeBoundingBox();
+            var bounds = letters.boundingBox;
+            var lettersWidth = bounds.max.x - bounds.min.x;
+            if (lettersWidth > canvas.width) {
+                var scaleFactor = canvas.width / lettersWidth;
+                letters.applyMatrix(new THREE.Matrix4().makeScale(
+                    scaleFactor,
+                    scaleFactor, 
+                    1
+                ));
+                letters.computeBoundingBox()
+                bounds = letters.boundingBox;
+            }
+            letters.applyMatrix( new THREE.Matrix4().makeTranslation(
+                (canvas.width - (bounds.max.x - bounds.min.x)) * 0.5,
+                (canvas.height - (bounds.max.y - bounds.min.y)) * 0.5,
+                0));
+            particles = new Particles(canvas, 1024, letters, 3).draw().start();
+            controller = new Controller(particles);
+            new FPS(particles);
+            updateCount();
+        }
+    );
 });
