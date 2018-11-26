930a8659f689027a81ded6f2e7bf3af5c9429bd9
Aditi Srinivasan <aditisri@mit.edu>
2018-11-25 21:33:17 -0500

    cleaned scale and translate

--- js/main.js
+++ js/main.js
@@ -20,6 +20,40 @@ function updateCount() {
     $('.count').text(comma(count));
 }
 
+function scaleLetters(letters, factor) {
+    letters.applyMatrix(new THREE.Matrix4().makeScale(
+        factor,
+        factor, 
+        1
+    )); 
+}
+
+function scaleToFit(letters, canvas) {
+    letters.computeBoundingBox();
+    var bounds = letters.boundingBox;
+    // scale width first
+    var lettersWidth = bounds.max.x - bounds.min.x;
+    if (lettersWidth > canvas.width) {
+        var scaleFactor = canvas.width / lettersWidth;
+        scaleLetters(letters, scaleFactor);
+    } 
+    // letters.computeBoundingBox()
+    // bounds = letters.boundingBox;
+    // // scale height second
+    // var lettersHeight = bounds.max.y - bounds.min.y;
+    // if (lettersHeight > canvas.height) {
+    //     var scaleFactor = canvas.height / lettersHeight;
+    //     scaleLetters(letters, scaleFactor);
+    // }
+}
+
+function translate(letters, deltaX, deltaY) {
+    letters.applyMatrix( new THREE.Matrix4().makeTranslation(
+        deltaX,
+        deltaY,
+        0));
+}
+
 $(document).ready(function() {
     var canvas = $('#display')[0];
     var loader = new THREE.FontLoader();
@@ -29,23 +63,10 @@ $(document).ready(function() {
             var letters = new THREE.TextGeometry("ADITI SAYS HI", {
                 font: font 
             });
-            letters.computeBoundingBox();
+            scaleToFit(letters, canvas);
+            letters.computeBoundingBox()
             var bounds = letters.boundingBox;
-            var lettersWidth = bounds.max.x - bounds.min.x;
-            if (lettersWidth > canvas.width) {
-                var scaleFactor = canvas.width / lettersWidth;
-                letters.applyMatrix(new THREE.Matrix4().makeScale(
-                    scaleFactor,
-                    scaleFactor, 
-                    1
-                ));
-                letters.computeBoundingBox()
-                bounds = letters.boundingBox;
-            }
-            letters.applyMatrix( new THREE.Matrix4().makeTranslation(
-                (canvas.width - (bounds.max.x - bounds.min.x)) * 0.5,
-                (canvas.height - (bounds.max.y - bounds.min.y)) * 0.5,
-                0));
+            translate(letters, (canvas.width - (bounds.max.x - bounds.min.x)) * 0.5, (canvas.height - (bounds.max.y - bounds.min.y)) * 0.5);
             particles = new Particles(canvas, 1024, letters, 3).draw().start();
             controller = new Controller(particles);
             new FPS(particles);
