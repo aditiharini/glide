fef8d5048816fcd52d5ff2c7da1502e2648fa419
Rose E Wang <rewang@Roses-MacBook-Pro.local>
2018-12-04 19:21:20 -0500

    [edited] orthographic -> perspective

--- js/main.js
+++ js/main.js
@@ -36,8 +36,9 @@ HEIGHT = window.innerHeight;
 
 $(document).ready(function() {
     scene = new THREE.Scene();
-    camera = new THREE.OrthographicCamera(WIDTH/-2, WIDTH/2, HEIGHT/2, HEIGHT/-2, 1, 1000);
-    camera.position.z = 30
+    // camera = new THREE.OrthographicCamera(WIDTH/-2, WIDTH/2, HEIGHT/2, HEIGHT/-2, 1, 1000);
+    camera = new THREE.PerspectiveCamera(140, WIDTH/HEIGHT, 1, 1000);
+    camera.position.set( 0.0, 200, 300);
     renderer = new THREE.WebGLRenderer();
     renderer.setClearColor(new THREE.Color(0, 0, 0));
 	renderer.setSize(WIDTH, HEIGHT);
